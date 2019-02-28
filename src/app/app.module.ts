7312e037ec4a1a78b2f0dc54ee6f89d6dd09c290
Kevin Ure <kevinure@Kevin-Ures-MacBook-Pro.local>
2019-02-28 10:38:59 -0500

    fuckit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarModule } from 'portal-navbar';
+import { NavbarModule } from 'micourt-common-ui';
 
 @NgModule({
   declarations: [
